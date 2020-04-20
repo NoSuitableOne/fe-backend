2f7d180995c327b90fca9f9b172c81782fa9b148
mayx <mayx@317hu.com>
2020-04-20 16:05:32 +0800

    支持所有图都能展示数字

--- app/services/echarts.js
+++ app/services/echarts.js
@@ -9,6 +9,14 @@ const types = {
   "lines": "line"
 };
 
+const lineItemStyle = {
+  normal: {
+    label: {
+      show: true
+    }
+  }
+};
+
 function formatter (value) {
   let ret = "";
   let maxLength = 5;
@@ -38,6 +46,7 @@ exports.generate = ({ id: filename, option}) => {
   const { type, xData, yData, xName, yName, title, label, custom = {} } = option;
   const { averageLine, showXValue, showMinMax } = custom;
   const echartsOption = Object.assign({}, {}, echartsConfig[type]);
+  let xDataMaxString = xData.reduce((last, ele) => ele.length > last? ele.length : last  , 0);
   if (type === 'bars' || type === 'lines') {
     echartsOption.xAxis.data = xData;
     echartsOption.series = [];
@@ -63,6 +72,13 @@ exports.generate = ({ id: filename, option}) => {
               fontSize: 12
             }
           };
+        }
+      }
+      if (type === 'lines') {
+        if (showXValue) {
+          echartsOption.series[echartsOption.series.length - 1].itemStyle = lineItemStyle;
+        } else {
+          echartsOption.series[echartsOption.series.length - 1].itemStyle = false;
         }
       }
     });
@@ -101,6 +117,25 @@ exports.generate = ({ id: filename, option}) => {
           }
         };
       }
+      if (showXValue) {
+        echartsOption.series[echartsOption.series.length - 1].label = {
+          show: true, // 开启显示
+          position: 'top', // 在上方显示
+          distance: 12,
+          verticalAlign: 'middle',
+          textStyle: { // 数值样式
+            color: 'black',
+            fontSize: 12
+          }
+        };
+      }
+    }
+    if (type === 'line') {
+      if (showXValue) {
+        echartsOption.series[echartsOption.series.length - 1].itemStyle = lineItemStyle;
+      } else {
+        echartsOption.series[echartsOption.series.length - 1].itemStyle = false;
+      }
     }
     if (averageLine) {
       echartsOption.series[echartsOption.series.length - 1].markLine = {
@@ -137,7 +172,7 @@ exports.generate = ({ id: filename, option}) => {
   } else {
     echartsOption.xAxis.name = '';
   }
-  if (xData.length * echartsOption.series.length >= 8 ) {
+  if (xDataMaxString > 6 || xData.length * echartsOption.series.length >= 8 ) {
     echartsOption.xAxis.axisLabel.formatter = formatter;
   }
   // y轴名字
